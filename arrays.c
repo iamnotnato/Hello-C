6fe644c86a4f86c2ad5de7bfe8f2535b5a6fd546
Alan Nato <alanwnato@gmail.com>
2022-12-21 15:31:53 -0800

    Arrays

--- arrays.c
+++ arrays.c
@@ -4,3 +4,12 @@ int  values[4] = {0, 1, 2, 3};
 char greetings[5] = {'h', 'e', 'l', 'l', 'o'};
 char name[] = {'f', 'r', 'e', 'd'};
 int bigarray[] = {[0] = 1, [1] = 10, [999] = 0};
+
+/**
+*Accessing Arrays
+*/
+
+int values[100];
+values[23] = 911;
+int j = values[23];
+values[j] = 912;
