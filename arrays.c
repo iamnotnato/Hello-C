1b53b4efcd4a6e262987545205eb647b1dcbd481
Alan Nato <alanwnato@gmail.com>
2022-12-21 15:41:50 -0800

    Arrays

--- arrays.c
+++ arrays.c
@@ -13,3 +13,18 @@ int values[100];
 values[23] = 911;
 int j = values[23];
 values[j] = 912;
+
+/**
+ * Multidemensional arrays
+ */
+
+int values [2][2];
+int values [2][2] = {{0, 1} , {1, 0}};
+int values [2][2] = {0, 1, 1, 0};
+
+/**
+Accessing multidemnsional arrays
+*/
+
+int i = values [2][3];
+values[0][2] = 1000;
